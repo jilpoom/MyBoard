d2b02b4cef2b48c5f616da58ca561a2ce9731113
JJY <wjddyd94@naver.com>
2022-08-14 14:05:31 +0900

    Fixed

--- myboard/settings.py
+++ myboard/settings.py
@@ -26,7 +26,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-qinh7i$q#%!rm584y*5e5kyz-7fqz#tn1_-)i*s8o1n&j5nnkv'
 SECRET_KEY = os.environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
